52a435e40ed47ce062cc8a2a6cbfa12324f45093
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-06-04 11:54:18 +0300

    deployment

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     general configuration parent class
     '''
     MOVIE_API_BASE_URL='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    SECRET_KEY= os.environ.get('23a36')
+    MOVIE_API_KEY=os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY= os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
--- test/test_movie.py
+++ test/test_movie.py
@@ -10,7 +10,19 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.or/t/p/w500/w500/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
+
+    def test_init(self):
+        '''
+        test case to confirm the movie object is instantiated correctly
+        '''
+        self.assertEqual(self.new_movie.id,1234)
+        self.assertEqual(self.new_movie.title,'python must be crazy')
+        self.assertEqual(self.new_movie.overview,'A thrilling new python series')
+        self.assertEqual(self.new_movie.poster,'https://image.tmdb.or/t/p/w500/w500/khsjha27hbs')
+        self.assertEqual(self.new_movie.vote_average,8.5)
+        self.assertEqual(self.new_movie.vote_count,129993 )
+
